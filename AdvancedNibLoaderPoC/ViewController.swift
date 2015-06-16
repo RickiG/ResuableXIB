63c1d804e046840ec997dac620ab1224e04eebb6
Ricki Gregersen <hi@rickigregersen.com>
2015-06-16 14:17:29 +0200

    Initial Commit

---
+++ AdvancedNibLoaderPoC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvancedNibLoaderPoC
+//
+//  Created by Ricki Gregersen on 16/06/15.
+//  Copyright (c) 2015 youandthegang.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
